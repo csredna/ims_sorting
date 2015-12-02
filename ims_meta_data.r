3f6838cc3e345ea028fffadd591233ff44cf2e5c
csredna <christiansen.anders@gmail.com>
2015-12-02 14:29:19 +0100

    The ims_meta_data.r file
    R file for extracting meta data from ims files.

---
+++ ims_meta_data.r
@@ -0,0 +1,31 @@
+#set wd to IMS repository
+
+b <- list.files(pattern = "ims.csv", full.names = TRUE)
+
+readdata <- function(filename){
+  
+  c <- read.table(file = filename, sep = "\t", quote = "", nrows = 10, skip = 6, comment.char = "", colClasses = "character")
+  
+  temp <- t(c)[c(1,2,3,8,9,10)]
+  #reading decired lines
+  idiot <- matrix(unlist(strsplit(t(temp), split= ",")), ncol = 3, byrow = TRUE)[,3]
+  #Getting values
+  names(idiot) <- matrix(unlist(strsplit(t(temp), split= ",")), ncol = 3, byrow = TRUE)[,2]
+  #well should return the descritions of values, works but does not pass it on :-(
+  return(idiot)
+  #made me feel like an idiot for not beeing smarter.
+  
+  
+  
+}
+
+table.o.data <- lapply(X = b, FUN  = readdata)
+#hate this function
+
+
+
+table.o.data <- as.data.frame(matrix(unlist(table.o.data), ncol = 6, byrow = TRUE))
+#is this the best, i think not ..:-(
+
+write.csv2(table.o.data, "metadata.csv")
+#yes im a bastard using csv2
